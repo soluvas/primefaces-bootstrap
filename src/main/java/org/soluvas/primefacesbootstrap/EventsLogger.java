17e70c4b658c2ca113caab211c2a0ca77e70b2f2
Hendy Irawan <hendy@soluvas.com>
2012-06-01 04:44:31 +0700

    Pretty happy with this now :-)

---
+++ src/main/java/org/soluvas/primefacesbootstrap/EventsLogger.java
@@ -0,0 +1,61 @@
+package org.soluvas.primefacesbootstrap;
+
+import org.atmosphere.cpr.AtmosphereResourceEvent;
+import org.atmosphere.websocket.WebSocketEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EventsLogger implements WebSocketEventListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(EventsLogger.class);
+
+    public EventsLogger() {
+    }
+
+    public void onSuspend(final AtmosphereResourceEvent event) {
+        logger.info("onSuspend(): {}:{}", event.getResource().getRequest().getRemoteAddr(),
+                event.getResource().getRequest().getRemotePort());
+    }
+
+    public void onResume(AtmosphereResourceEvent event) {
+        logger.info("onResume(): {}:{}", event.getResource().getRequest().getRemoteAddr(),
+                event.getResource().getRequest().getRemotePort());
+    }
+
+    public void onDisconnect(AtmosphereResourceEvent event) {
+        logger.info("onDisconnect(): {}:{}", event.getResource().getRequest().getRemoteAddr(),
+                event.getResource().getRequest().getRemotePort());
+    }
+
+    public void onBroadcast(AtmosphereResourceEvent event) {
+        logger.info("onBroadcast(): {}", event.getMessage());
+    }
+
+    public void onThrowable(AtmosphereResourceEvent event) {
+        logger.warn("onThrowable(): {}", event);
+    }
+
+    public void onHandshake(WebSocketEvent event) {
+        logger.info("onHandshake(): {}", event);
+    }
+
+    public void onMessage(WebSocketEvent event) {
+        logger.info("onMessage(): {}", event);
+    }
+
+    public void onClose(WebSocketEvent event) {
+        logger.info("onClose(): {}", event);
+    }
+
+    public void onControl(WebSocketEvent event) {
+        logger.info("onControl(): {}", event);
+    }
+
+    public void onDisconnect(WebSocketEvent event) {
+        logger.info("onDisconnect(): {}", event);
+    }
+
+    public void onConnect(WebSocketEvent event) {
+        logger.info("onConnect(): {}", event);
+    }
+}
---
+++ src/main/java/org/soluvas/primefacesbootstrap/MeteorChat.java
@@ -0,0 +1,88 @@
+package org.soluvas.primefacesbootstrap;
+
+import static org.atmosphere.cpr.AtmosphereResource.TRANSPORT.LONG_POLLING;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.atmosphere.cpr.AtmosphereResourceEventListenerAdapter;
+import org.atmosphere.cpr.BroadcasterFactory;
+import org.atmosphere.cpr.DefaultBroadcaster;
+import org.atmosphere.cpr.Meteor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soluvas.json.JsonUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class MeteorChat extends HttpServlet {
+	
+	private transient Logger log = LoggerFactory.getLogger(MeteorChat.class);
+	
+	/**
+	 * Create a {@link Meteor} and use it to suspend the response.
+	 * 
+	 * @param req
+	 *            An {@link HttpServletRequest}
+	 * @param res
+	 *            An {@link HttpServletResponse}
+	 */
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws IOException {
+		// Set the logger level to TRACE to see what's happening.
+		Meteor m = Meteor.build(req).addListener(
+				new AtmosphereResourceEventListenerAdapter());
+
+		m.resumeOnBroadcast(m.transport() == LONG_POLLING ? true : false)
+				.suspend(-1);
+	}
+
+	/**
+	 * Re-use the {@link Meteor} created on the first GET for broadcasting
+	 * message.
+	 * 
+	 * @param req
+	 *            An {@link HttpServletRequest}
+	 * @param res
+	 *            An {@link HttpServletResponse}
+	 */
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws IOException {
+		String body = IOUtils.toString(req.getReader());
+		log.info("Input: {}", body);
+		Data data;
+		if (body.isEmpty())
+			data = new Data("Halo", "Yuhu");
+		else
+			data = new ObjectMapper().readValue(body, Data.class);
+		log.info("Broadcasting {}", data);
+		BroadcasterFactory.getDefault().lookup(DefaultBroadcaster.class, "/*")
+				.broadcast(data.toString());
+	}
+
+	public final static class Data implements Serializable {
+
+		public String text;
+		public String author;
+
+		public Data() {
+			super();
+		}
+		
+		public Data(String author, String text) {
+			this.author = author;
+			this.text = text;
+		}
+
+		public String toString() {
+			return JsonUtils.asJson(this);
+		}
+	}
+}
